e188f885723a2b13c81e42732d02f6ff1ff2c678
Musawar <imusawar@gmail.com>
2015-06-09 14:51:20 -0700

    my first commit

--- ChatClientSwift/ChatClientAppDelegate.swift
+++ ChatClientSwift/ChatClientAppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  ChatClientSwift
 //
-//  Created by Attu SmArty on 5/29/15.
-//  Copyright (c) 2015 skylite. All rights reserved.
+//  Created by Musawar Ahmed on 5/29/15.
+//  Copyright (c) 2015 musawar. All rights reserved.
 //
 
 import UIKit
